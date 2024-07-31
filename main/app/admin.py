37776ab35300b501b3d239e9f0068a9cc62db4dd
snezhanaprog <snezanacvetkova9745@gmail.com>
2024-07-31 10:01:44 +0300

    change backend logic - admin, models, urls, serializers, views, settings

--- main/app/admin.py
+++ main/app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+admin.site.register(Budget)
+admin.site.register(SavingGoal)
+admin.site.register(Expense)
+admin.site.register(Income)
