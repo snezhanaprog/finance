ac003f4fd8ea7ae521f7c0de364b8fd81304dc6c
snezhanaprog <snezanacvetkova9745@gmail.com>
2024-07-31 10:02:25 +0300

    add frontend logic

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SavingGoal  from './components/SavingGoal.js'
+
+function App() {
+  return (
+    <div className="App">
+        <SavingGoal/>
+    </div>
+  );
+}
+
+export default App;
